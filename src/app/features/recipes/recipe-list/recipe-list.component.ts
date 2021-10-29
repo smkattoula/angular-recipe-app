e86acb7d30105abcea8bbbdad2e310b710a008ea
Shaker Kattoula <smkattoula@gmail.com>
2021-10-29 15:20:51 -0700

    create recipe-item component template, onest in recipe-list component, loop through recipe array and output a recipe-item for each recipe

--- src/app/features/recipes/recipe-list/recipe-list.component.ts
+++ src/app/features/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://live.staticflickr.com/65535/50889476642_a1d1192cc0_b.jpg'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://live.staticflickr.com/65535/50889476642_a1d1192cc0_b.jpg'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://live.staticflickr.com/65535/50889476642_a1d1192cc0_b.jpg'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://live.staticflickr.com/65535/50889476642_a1d1192cc0_b.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
