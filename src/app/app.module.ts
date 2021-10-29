c1452144a561152216f65238e6613736eb7179a5
Shaker Kattoula <smkattoula@gmail.com>
2021-10-28 19:39:38 -0700

    generate app components, display components in UI, and create navbar via bootstrap in the header template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ShoppingListComponent } from './features/shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './features/shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipeDetailComponent } from './features/recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './features/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './features/recipes/recipe-item/recipe-item.component';
+import { HeaderComponent } from './components/header/header.component';
+import { IngredientComponent } from './components/ingredient/ingredient.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { RecipesComponent } from './features/recipes/recipes.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    IngredientComponent,
+    RecipeComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
