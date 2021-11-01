c941eec024c3bcf8182ad644876b8fb6fe889ad0
Shaker Kattoula <smkattoula@gmail.com>
2021-11-01 16:54:28 -0700

    add simple navigation between recipes and shopping list using event binding/ngIf

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureEvent = new EventEmitter<string>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSelect(feature: string) {
+    this.featureEvent.emit(feature);
   }
-
 }
