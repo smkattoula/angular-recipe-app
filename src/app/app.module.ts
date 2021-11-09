b94fd2b1d9caa08a9aae9ace6d165c9964ad9903
Shaker Kattoula <smkattoula@gmail.com>
2021-11-09 10:10:36 -0800

    create dropdown directive and add it as an atttribute to the dropdown menus in the templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { IngredientComponent } from './components/ingredient/ingredient.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { RecipesComponent } from './features/recipes/recipes.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { RecipesComponent } from './features/recipes/recipes.component';
     IngredientComponent,
     RecipeComponent,
     HeaderComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  private isOpen = false;
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+  @HostListener('click')
+  onClick() {
+    const dropdown = this.elementRef.nativeElement.nextElementSibling;
+    if (!this.isOpen) {
+      this.renderer.addClass(dropdown, 'show');
+    } else {
+      this.renderer.removeClass(dropdown, 'show');
+    }
+    this.isOpen = !this.isOpen;
+  }
+}
