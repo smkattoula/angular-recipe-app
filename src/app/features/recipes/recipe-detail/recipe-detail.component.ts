8522a8677dadf2af1ab1cdf0479eb4a2ec1af0ab
Shaker Kattoula <smkattoula@gmail.com>
2021-11-15 17:31:31 -0800

    use alternate service solution to populate shopping list with ingredients from the recipe-item

--- src/app/features/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/features/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,16 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { ShoppingListService } from 'src/app/services/shopping-list.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 import { Recipe } from '../recipe-list/recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe
-  constructor() { }
+  @Input() recipe: Recipe;
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAddIngredientsToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    console.log(this.recipe.ingredients);
   }
-
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../features/recipes/recipe-list/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,28 +12,24 @@ export class RecipeService {
     new Recipe(
       'Test Recipe 1',
       'This is a test recipe',
-      'https://live.staticflickr.com/65535/50889476642_a1d1192cc0_b.jpg'
+      'https://live.staticflickr.com/65535/50889476642_a1d1192cc0_b.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'Test Recipe 2',
       'This is a test recipe',
-      'https://live.staticflickr.com/65535/50889476642_a1d1192cc0_b.jpg'
-    ),
-    new Recipe(
-      'Test Recipe 3',
-      'This is a test recipe',
-      'https://live.staticflickr.com/65535/50889476642_a1d1192cc0_b.jpg'
-    ),
-    new Recipe(
-      'Test Recipe 4',
-      'This is a test recipe',
-      'https://live.staticflickr.com/65535/50889476642_a1d1192cc0_b.jpg'
+      'https://live.staticflickr.com/65535/50889476642_a1d1192cc0_b.jpg',
+      [new Ingredient('Burger', 1), new Ingredient('Milk Shake', 2)]
     ),
   ];
 
-  constructor() {}
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
